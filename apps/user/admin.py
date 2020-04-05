6c16351cc158b0c5d05ed3cb6c95049c5463dce6
Iamacode <mahongfei_mhf@163.com>
2020-04-05 11:07:35 +0800

    "add user manage"

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import *
+from .models import Ouser
 
 
 # Register your models here.
 
 class OuserAdmin(Ouser):
-    list_display = ('link',)
+    list_display = ('link', 'username', 'date_joined')
+
+
+admin.site.register(Ouser)
